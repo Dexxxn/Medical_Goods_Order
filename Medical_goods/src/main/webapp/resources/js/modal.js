52fee89acd3ee856cf92bf3ab5098fd037928160
GR <GR@GRN-304-05>
2023-06-15 15:57:00 +0900

    모달 수정

--- Medical_goods/src/main/webapp/resources/js/modal.js
+++ Medical_goods/src/main/webapp/resources/js/modal.js
@@ -59,39 +59,6 @@ function useList(url) {
         // Contents 영역 삭제
 
         // Contents 영역 교체
-    	$(".basicB.top").append("<input type='button' id='modalOpen' value='등록'>");
-        // 모달 창 열기
-        $(document).on("click", "#modalOpen", function() {
-            // 비동기식으로 모달 페이지를 불러와서 열기
-            $.ajax({
-                url: "/modal",
-                async: true,
-                cache: false
-            }).done(function(modalContent) {
-                // 모달 페이지를 추가한 후 열기
-                $("body").append(modalContent);
-                $(".modal1").modal("show");
-                $(".modal-overlay").modal("show");
-                // 삭제
-                $("#oo").remove();
-            });
-        });
-/*    	// 모달 열기
-    	$(document).ready(function() {
-    		$("#modal").click(function() {
-    			$(".modal1").show();
-    			$(".modal-overlay").show();
-    		});
-    	});
-    	// 모달 닫기
-    	$(document).ready(function() {
-    		// x 아이콘 클릭 시 모달 닫기
-    		$(".closeB").click(function() {
-    			$(".modal1").hide();
-    			$(".modal-overlay").hide();
-    		});
-    	});*/
-    	
         //console.log(data)
         var str = "";
         
@@ -111,6 +78,53 @@ function useList(url) {
         // tbody 값 대체
         $(".table.only tbody").html(str);
         
+        // 등록 버튼 생성
+    	$(".basicB.top").append("<input type='button' id='modalOpen' value='등록'>");
+    	// 모달 창 열기
+        $(document).on("click", "#modalOpen", function() {
+            // 비동기식으로 모달 페이지를 불러와서 열기
+            $.ajax({
+                url: "/modal",
+                async: true,
+                cache: false
+            }).done(function(modalContent) {
+                // 모달 페이지를 추가한 후 열기
+                $("body").append(modalContent);
+                $(".modal1").modal("show");
+                $(".modal-overlay").show();
+                // 모달 닫고 요소 삭제(모달 재생성위해)
+                $(".closeB").click(function() {
+                    $(".modal1").hide();
+                    $(".modal-overlay").hide();
+                    $(".modal1").remove(); // 모달 페이지 삭제
+                    $(".modal-overlay").remove(); // 모달 오버레이 삭제
+                });
+                // table.jsp 삭제
+                $("#oo").remove();
+                // thead에 열 추가
+                $(".table.only thead tr").append("<th>특이사항</th>");
+                // tbody 값 대체
+                $(".table.only tbody").html(str);
+            });
+        });
     });
 
 }
+
+/*    	
+// 모달 열기
+$(document).ready(function() {
+	$("#modal").click(function() {
+		$(".modal1").show();
+		$(".modal-overlay").show();
+	});
+});
+// 모달 닫기
+$(document).ready(function() {
+	// x 아이콘 클릭 시 모달 닫기
+	$(".closeB").click(function() {
+		$(".modal1").hide();
+		$(".modal-overlay").hide();
+	});
+});
+*/
