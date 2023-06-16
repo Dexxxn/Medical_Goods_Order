93b696a95472c47eecd7055bd885eadb6ff46abf
GR <GR@GRN-304-05>
2023-06-16 16:09:10 +0900

    모달창 재생성시 오류 수정, 수량 입력 가능하게 추가

--- Medical_goods/src/main/webapp/resources/js/modal.js
+++ Medical_goods/src/main/webapp/resources/js/modal.js
@@ -46,7 +46,7 @@ function invenList(url) {
 
 }
 
-// 사용 입력 품목 리스트
+//사용 입력 품목 리스트
 function useList(url) {
     // Ajax 요청 보내기
     $.ajax({
@@ -67,7 +67,7 @@ function useList(url) {
             str += "<td>" + data[i].item_id + "</td>";
             str += "<td>" + data[i].item_name + "</td>";
             str += "<td>" + data[i].unit + "</td>";
-            str += "<td>" + data[i].quantity_used + "</td>";
+            str += "<td id='quantityUsedCell'>" + data[i].quantity_used + "</td>";
             str += "<td>" + data[i].significant + "</td>";
             str += "</tr>";
         }
@@ -80,7 +80,7 @@ function useList(url) {
         
         // 등록 버튼 생성
     	$(".basicB.top").append("<input type='button' id='modalOpen' value='등록'>");
-    	// 모달 창 열기
+    	// 사용수량등록 모달 창 열기
         $(document).on("click", "#modalOpen", function() {
             // 비동기식으로 모달 페이지를 불러와서 열기
             $.ajax({
@@ -90,11 +90,11 @@ function useList(url) {
             }).done(function(modalContent) {
                 // 모달 페이지를 추가한 후 열기
                 $("body").append(modalContent);
-                $(".modal1").modal("show");
+                $(".modal1").show();
                 $(".modal-overlay").show();
                 // 모달 닫고 요소 삭제(모달 재생성위해)
                 $(".closeB").click(function() {
-                    $(".modal1").hide();
+                	$(".modal1").hide();
                     $(".modal-overlay").hide();
                     $(".modal1").remove(); // 모달 페이지 삭제
                     $(".modal-overlay").remove(); // 모달 오버레이 삭제
@@ -102,9 +102,11 @@ function useList(url) {
                 // table.jsp 삭제
                 $("#oo").remove();
                 // thead에 열 추가
-                $(".table.only thead tr").append("<th>특이사항</th>");
+                $(".modal1 .table.only thead tr").append("<th>특이사항</th>");
                 // tbody 값 대체
-                $(".table.only tbody").html(str);
+                $(".modal1 .table.only tbody").html(str);
+                // 사용수량 셀을 입력 요소로 변경
+                $(".modal1 #quantityUsedCell").html("<input type='text' name='quantity_used' value='0'>");
             });
         });
     });
